d42c18132acd5b775b92776dfff0eaae2dc32038
azaniapvtltd <azaniapvtltd@gmail.com>
2024-03-05 11:55:33 +0530

    mogno setup completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
-   const app = require('./app')
+const app = require("./app");
+const dotenv = require("dotenv");
+const path = require("path");
+const dotenvConfig = dotenv.config({
+  path: path.resolve(__dirname, "./config", "config.env"),
+});
 
-const server= app.listen(8000, ()=>{
-    console.log('server is listening')
-})
+const mongoose = require("mongoose");
+console.log(process.env.MONGO);
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("database connected");
+    app.listen(process.env.PORT, () => {
+      console.log(`server connected at ${process.env.PORT}`);
+    });
+  });
