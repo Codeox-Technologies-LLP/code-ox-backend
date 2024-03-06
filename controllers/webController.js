2331901a12f64caa0030ce9a1a445444a9c927d0
nihal <nihalpuliyakkady@gmail.com>
2024-03-06 10:41:37 +0530

    status_code update

--- controllers/webController.js
+++ controllers/webController.js
@@ -1,71 +1,64 @@
 const Content = require('../models/webModel');
 
 //post
-
 exports.createContent = async (req, res) => {
   try {
-    if (!req.file || req.file.length === 0) {
-      return res.status(400).json({ message: 'No files were uploaded.' });
-    }
-    
-    
-    const image = req.file.path ;
-
-    const newContent = await Content.create({
-      heading: req.body.heading,
-      subheading: req.body.subheading,
-      description: req.body.description,
-      buttonLink: req.body.buttonLink,
-      image: image,
-    });
-
-    res.status(201).json(newContent);
+      if (!req.file || req.file.length === 0) {
+          return res.status(400).json({ success: false, message: 'No files were uploaded.' });
+      }
+      const image = req.file.path;
+      const newContent = await Content.create({
+          heading: req.body.heading,
+          subheading: req.body.subheading,
+          description: req.body.description,
+          buttonLink: req.body.buttonLink,
+          image: image,
+      });
+      res.status(201).json({ success: true, statusCode:200, message: 'Content created successfully', data: newContent });
   } catch (err) {
-    res.status(400).json({ message: err.message });
+      res.status(500).json({ success: false, message: err.message });
   }
 };
 
+
 //get
 exports.getAllContent = async (req, res) => {
-    try {
+  try {
       const allContent = await Content.find();
-      res.status(200).json(allContent);
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  };
+      res.status(200).json({   statusCode:200, success: true, message: "Successfully retrieved all content",  data: allContent });
+  } catch (err) {
+      res.status(500).json({ success: false, message: err.message });
+  }
+};
 
   //update
   exports.updateContent = async (req, res) => {
     try {
-      const existingContent = await Content.findOne({ _id: req.params.id });
-  
-      if (existingContent) {
-       
-        existingContent.set(req.body);
-        const updatedContent = await existingContent.save();
-        res.status(200).json(updatedContent);
-      } else {
-    
-        const newContent = await Content.create(req.body);
-        res.status(201).json(newContent);
-      }
+        const updatedContent = await Content.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+        if (updatedContent) {
+            res.status(200).json({ statusCode:200,success: true, message: "Content updated successfully", data: updatedContent });
+        } else {
+            res.status(404).json({ success: false, message: "Content not found" });
+        }
     } catch (err) {
-      res.status(400).json({ message: err.message });
+        res.status(500).json({ success: false, message: err.message });
     }
-  };
+};
+
 
 
   //delete
   exports.deleteContent = async (req, res) => {
     try {
-      const deletedContent = await Content.findByIdAndDelete(req.params.id);
-      if (!deletedContent) {
-        return res.status(404).json({ message: 'Content not found' });
-      }
-      res.status(200).json({ message: 'Content deleted successfully' });
+        const deletedContent = await Content.findByIdAndDelete(req.params.id);
+        if (!deletedContent) {
+            return res.status(404).json({ success: false, message: 'Content not found' });
+        }
+        res.status(200).json({ success: true, statusCode:200, message: 'Content deleted successfully' });
     } catch (err) {
-      res.status(400).json({ message: err.message });
+        res.status(500).json({ success: false,  message: err.message  });
     }
-  };
+};
+
   
--- routes/webPage.js
+++ routes/webPage.js
@@ -5,26 +5,43 @@ const contentController = require('../controllers/webController');
 
 ///multer storage configuration 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads'); // Directory where uploaded files will be stored
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Rename file with current timestamp to avoid conflicts
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+  }
+});
+
+// Error handling middleware
+function handleMulterErrors(err, req, res, next) {
+  if (err instanceof multer.MulterError) {
+    if (err.code === 'LIMIT_FILE_SIZE') {
+      return res.status(400).json({ message: 'File size exceeds the limit (2MB)' });
     }
-  });
+  }
+  next(err);
+}
 
-  const upload = multer({ storage: storage });
 
 
-  router.post('/content', upload.single('image'), contentController.createContent);
+router.post('/content', upload.single('image'), contentController.createContent);
 
 router.get('/content', contentController.getAllContent);
 
 router.put('/content/:id', contentController.updateContent);
 
 
+
 router.delete('/content/:id', contentController.deleteContent);
 
 
-
+router.use(handleMulterErrors)
 module.exports = router;
