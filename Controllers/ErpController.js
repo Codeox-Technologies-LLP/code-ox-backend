d5e144441bfd0bced782cbb6a7fb20f8720ca145
nihal <nihalpuliyakkady@gmail.com>
2024-03-25 14:31:30 +0530

    dlt-erp

--- Controllers/ErpController.js
+++ Controllers/ErpController.js
@@ -79,50 +79,24 @@ const updateProjects = async (req, res) => {
  };
 
 
-// const updateProjects = async (req, res) => {
-//    try {
-//       const id = req.params.id;
-//       if (!mongoose.isValidObjectId(id)) {
-//           return res.status(400).json({ statusCode: 400, message: 'Invalid Id' });
-//       }
-//       const {  name , description} = req.body;
-//       const  image  = req.file?.path;
-//       const baseUrl = `${req.protocol}://${req.get('host')}/${image.replace(/\\/g, "/")}`;
-//       const update = {
-//          image: baseUrl,
-//          name,
-//          description
-//       };
 
-//       const response = await erpModel.findOneAndUpdate({_id:id}, update, { new: true })
-
-//       if (!response) {
-//          return res.status(404).json({ statusCode: 404, message: 'Client not found' });
-//      }
-
-//       res.status(200).json({ statusCode: 200, message: 'erp projects updated successfully' ,response})
-//    } catch (error) {
-//       res.status(500).json({ statusCode: 500, success: false, message: error.message })
-//    }
-// }
 
 //DELETE
 
 const deleteProjects = async (req, res) => {
    try {
-      const id = req.params.id;
-      if (!mongoose.isValidObjectId(id)) {
-         return res.status(400).json({ statusCode: 400, message: 'Invalid Id' });
+     const id = req.params.id;
+     if (!mongoose.isValidObjectId(id)) {
+       return res.status(400).json({ statusCode: 400, message: 'Invalid Id' ,success:false});
      }
-      const response = await erpModel.findOneAndDelete({_id:id});
-      if (!response){
-         res.status(200).json({ statusCode: 200, success: true, message: "no documents were deleted" });
-      }
-      res.status(200).json({ statusCode: 200, success: true, message: "deleting successful" });
-
+ 
+     await erpModel.findOneAndDelete({ _id: id });
+ 
+     res.status(200).json({ statusCode: 200, success: true, message: "deleting successful" });
+ 
    } catch (error) {
-      res.status(500).json({ statusCode: 500, success: false, message: error.message })
+     res.status(500).json({ statusCode: 500, success: false, message: error.message })
    }
-}
+ }
 
 module.exports = { addProjects, getProjects, updateProjects, deleteProjects }
