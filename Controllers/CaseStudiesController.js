716e207f44d50d1d162fa434a186e0e5a9484e14
nihal <nihalpuliyakkady@gmail.com>
2024-03-26 16:03:18 +0530

    case_studies_status_code

--- Controllers/CaseStudiesController.js
+++ Controllers/CaseStudiesController.js
@@ -5,12 +5,13 @@ const bgValidator = require('../middlewares/bg');
 const addCaseStudies = async (req, res) => {
   try {
     const { title, subtitle, caseStudiesDescription, link, category, bg } = req.body;
-
-    // Validate bg color format
     if (!bgValidator.isValidColorFormat(bg)) {
-      return res.status(400).json({ message: 'Invalid color format for bg field ', success: false });
+      return res.status(422).json({ message: 'Invalid color format for bg field ', success: false , statusCode:422});
     }
 
+    if (!title || !subtitle || !caseStudiesDescription || !link || !category || !bg || !req.file) {
+      return res.status(400).json({ message: 'Missing required fields', statusCode: 400, success: false });
+  }
     const { path: imagePath } = req.file;
     const baseUrl = `${req.protocol}://${req.get('host')}/${imagePath.replace(/\\/g, "/")}`;
     const newCaseStudy = new caseStudiesModel({
@@ -22,15 +23,11 @@ const addCaseStudies = async (req, res) => {
       category,
       bg
     });
-
-    // Save the new case study
     await newCaseStudy.save();
-
-    // Send success response with custom message
     res.status(200).json({ statusCode: 200, message: 'Case study added successfully', success: true });
   } catch (error) {
     console.error(error);
-    // Send error response
+
     res.status(500).json({ message: 'Server Error', statusCode: 500, success: false });
   }
 };
@@ -39,14 +36,11 @@ const getCaseStudies = async (req, res, next) => {
   try {
     const category = req.query.category ? req.query.category.toLowerCase() : null;
     let caseStudiesData = await caseStudiesModel.find();
-
-    // Filter the data if category is provided
     if (category) {
       caseStudiesData = caseStudiesData.filter(caseStudy =>
         caseStudy.category.toLowerCase() === category
       );
     }
-
     if (caseStudiesData.length === 0) {
       return res.status(404).json({ statusCode: 404, success: false, message: 'No case studies found for the provided category.' });
     }
@@ -61,7 +55,7 @@ const updateCaseStudies = async (req, res) => {
   try {
     const id = req.params.id;
     if (!mongoose.isValidObjectId(id)) {
-      return res.status(400).json({ statusCode: 400, message: 'Invalid Id' });
+      return res.status(400).json({ statusCode: 400, message: 'Invalid Id' , success:false });
     }
     const { title, subtitle, caseStudiesDescription, link, categories, bg } = req.body;
     const { category } = req.body;
@@ -76,17 +70,14 @@ const updateCaseStudies = async (req, res) => {
       link,
       bg
     };
-
     const updatedCaseStudies = await caseStudiesModel.findOneAndUpdate(
       { _id: id },
       update,
       { new: true }
     );
-
     if (!updatedCaseStudies) {
-      return res.status(404).json({ statusCode: 404, success:false, message: 'Case Studies not found' });
+      return res.status(404).json({ statusCode: 404, success: false, message: 'Case Studies not found' });
     }
-
     res.status(200).json({ statusCode: 200, success: true, message: 'Case studies updated successfully', updatedCaseStudies });
   } catch (error) {
     console.error(error);
@@ -98,13 +89,10 @@ const deleteCaseStudy = async (req, res) => {
   try {
     const id = req.params.id;
     if (!mongoose.isValidObjectId(id)) {
-      return res.status(400).json({ statusCode: 400, message: 'Invalid Id' ,success:false});
+      return res.status(400).json({ statusCode: 400, message: 'Invalid Id', success: false });
     }
-
     await caseStudiesModel.findOneAndDelete({ _id: id });
-
     res.status(200).json({ statusCode: 200, success: true, message: "deleting successful" });
-
   } catch (error) {
     res.status(500).json({ statusCode: 500, success: false, message: error.message })
   }
