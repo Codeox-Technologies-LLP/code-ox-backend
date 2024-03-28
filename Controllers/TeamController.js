2f9904b2d59411090cc304eb2363478b6022f68f
nihal <nihalpuliyakkady@gmail.com>
2024-03-28 14:57:19 +0530

    dlt_bug_teams

--- Controllers/TeamController.js
+++ Controllers/TeamController.js
@@ -1,10 +1,11 @@
-const teamModel = require('../Model/Team')
 const mongoose = require('mongoose');
+const teamModel = require('../Model/Team')
+
 //post
 const addTeam = async (req, res) => {
     try {
-        const { path: imagePath } = req.file; 
-        const baseUrl = `${req.protocol}://${req.get('host')}/${imagePath.replace(/\\/g, "/")}`;   
+        const { path: imagePath } = req.file;
+        const baseUrl = `${req.protocol}://${req.get('host')}/${imagePath.replace(/\\/g, "/")}`;
         if (!imagePath) {
             return res.status(400).json({ message: 'Image file is required' });
         }
@@ -22,8 +23,8 @@ const addTeam = async (req, res) => {
 // get
 const getTeam = async (req, res) => {
     try {
-        const data = await teamModel.find({})     
-        res.status(200).json({ statusCode: 200, success:true, message: 'Team fetched successfully', data: data })
+        const data = await teamModel.find({})
+        res.status(200).json({ statusCode: 200, success: true, message: 'Team fetched successfully', data: data })
     } catch (error) {
         res.status(500).json({ statusCode: 500, success: false, message: error.message })
     }
@@ -32,10 +33,10 @@ const getTeam = async (req, res) => {
 const updateTeam = async (req, res) => {
     try {
         const id = req.params.id;
-        const baseUrl = `${req.protocol}://${req.get('host')}/`;  
+        const baseUrl = `${req.protocol}://${req.get('host')}/`;
         const imagePath = req.file ? req.file.path.replace(/\\/g, "/") : '';
         const data = {
-            image: req.file ? baseUrl + imagePath : undefined, 
+            image: req.file ? baseUrl + imagePath : undefined,
             name: req.body.name,
             role: req.body.role
         };
@@ -43,7 +44,7 @@ const updateTeam = async (req, res) => {
         if (!response) {
             return res.status(404).json({ statusCode: 404, success: false, message: "No team found with the provided ID" });
         }
-        res.status(200).json({ statusCode: 200, success:true, message: 'team updated successfully', data: response });
+        res.status(200).json({ statusCode: 200, success: true, message: 'team updated successfully', data: response });
     } catch (error) {
         res.status(500).json({ statusCode: 500, success: false, message: error.message });
     }
@@ -51,17 +52,17 @@ const updateTeam = async (req, res) => {
 /// delete
 const deleteTeam = async (req, res) => {
     try {
-      const id = req.params.id;
-      if (!mongoose.isValidObjectId(id)) {
-        return res.status(400).json({ statusCode: 400, message: 'Invalid Id' ,success:false});
-      }
-      await teamModel.findOneAndDelete({ _id: id });
-      res.status(200).json({ statusCode: 200, success: true, message: "deleting successful" });
-  
+        const id = req.params.id;
+        if (!mongoose.isValidObjectId(id)) {
+            return res.status(400).json({ statusCode: 400, message: 'Invalid Id', success: false });
+        }
+        await teamModel.findOneAndDelete({ _id: id });
+        res.status(200).json({ statusCode: 200, success: true, message: "deleting successful" });
+
     } catch (error) {
-      res.status(500).json({ statusCode: 500, success: false, message: error.message })
+        res.status(500).json({ statusCode: 500, success: false, message: error.message })
     }
-  }
+}
 
 
 
