018907edf92d6813eba4199605bf16ff60253b17
nihal <nihalpuliyakkady@gmail.com>
2024-04-17 15:27:34 +0530

    cors_fix

--- server.js
+++ server.js
@@ -1,53 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const mongoose = require('mongoose');
-const router = require('./Routes/Routes');
-const multer = require('multer');
-const app = express();
-
-// Enable CORS middleware
-app.use(cors());
-
-// Parse JSON and URL-encoded request bodies
-app.use(express.json());
+const express = require('express')
+const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
+const  router  = require('./Routes/Routes');
+const multer = require('multer')
+const cors = require('cors')
+const app = express()
+app.use(cors({origin:'*'}))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-
-// Serve static files
+app.use('/api',router)
 app.use('/public', express.static(path.join(__dirname, 'public')));
+const dotenvConfig = dotenv.config({
+  path: path.resolve(__dirname, "./config", "config.env"),
+});
 
-// Routes
-app.use('/api', router);
-
-// Error handling middleware
 app.use((err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     // Handle Multer errors
     if (err.code === 'LIMIT_UNEXPECTED_FILE') {
-      return res.status(400).json({ statusCode: 400, success: false, message: 'Unexpected field: ' + err.field });
+      return res.status(400).json({ statusCode:400,success:false, message: 'Unexpected field: ' + err.field });
     }
     // Handle other Multer errors
-    return res.status(500).json({ statusCode: 500, success: false, message: 'Internal server error: ' + err.message });
+    return res.status(500).json({ statusCode:500,success:false,message: 'Internal server error: ' + err.message });
   } else if (err) {
     // Handle other errors
     console.error('Unexpected error:', err);
-    return res.status(500).json({ statusCode: 400, success: false, message: 'Internal server error: ' + err.message });
+    return res.status(500).json({ statusCode:400,success:false,message: 'Internal server error: ' + err.message });
   }
   next();
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log("Database Connected ");
-  // Start the server
-  app.listen(process.env.PORT, () => {
-    console.log(`Server Connected at ${process.env.PORT}`);
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected ");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Connected at ${process.env.PORT}`);
+    });
   });
-})
-.catch((err) => {
-  console.error("Database connection error:", err);
-});
